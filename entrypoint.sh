78d1d4339bce11433c434c9cadefff6f9ad8214e
Cimei <cimeibt@cnpq.br>
2023-09-05 10:32:34 -0300

    v3.2.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#exec gunicorn --config /app/gunicorn_config.py app:app --reload
+exec gunicorn --config /app/gunicorn_config.py app:app
 
-set -e
+# set -e
 
-# Function to start Gunicorn with dynamic reload-extra-file options
-start_gunicorn() {
-    # Generate the reload-extra-file options dynamically
-    extra_files=$(find /app -name "*.html" -printf "--reload-extra-file %p ")
+# # Function to start Gunicorn with dynamic reload-extra-file options
+# start_gunicorn() {
+#     # Generate the reload-extra-file options dynamically
+#     extra_files=$(find /app -name "*.html" -printf "--reload-extra-file %p ")
 
-    # Start Gunicorn
-    echo "Starting Gunicorn..."
-    gunicorn --config /app/gunicorn_config.py app:app --reload --reload-engine=poll $extra_files
-}
+#     # Start Gunicorn
+#     echo "Starting Gunicorn..."
+#     gunicorn --config /app/gunicorn_config.py app:app --reload --reload-engine=poll $extra_files
+# }
 
-# Start Gunicorn
-start_gunicorn
+# # Start Gunicorn
+# start_gunicorn
